ac88a33fdc686200aafc696f53bd796b44ae3ad2
Tanja Venborg <tanjavenborg@gmail.com>
2016-05-13 15:02:22 +0300

    no message

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function:
+
 ## So I'm trying to set up a matrix funtion, from where it should be possible to
 ## find the inverse matrix. From the example vector, I have basically just changed the 
-## mean into inverse. You want to use the <<- instead of the <- when assigning y to x because you are
+## mean into inverse. Note to myself; You want to use the <<- instead of the <- when assigning y to x because you are
 ## assigning a value to an object in a different environment that you are in currently.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -15,15 +13,16 @@ makeCacheMatrix <- function(x = matrix()) {
   }
   
   get <- function() x
-  setinverse <- function(mean) maxtrixinv <<- mean
+  setinverse <- function(solve) maxtrixinv <<- solve()
   getinverse <- function() matrixinv
   list(set= set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
 ## The first part should be able to return the inverted Matrix calculated previously. 
 ## The second part calculates the inverse Matrix, if it has not already been taken care of. 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   matrixinv <- x$getinverse()
