ba6b032ecda6a7e8eef1e73884736f2bf095f30a
Tanja Venborg <tanjavenborg@gmail.com>
2016-05-13 14:45:33 +0300

    no message

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function:
 ## So I'm trying to set up a matrix funtion, from where it should be possible to
-## find the mean of the inverse matrix. From the example vector, I have basically just changed the 
+## find the inverse matrix. From the example vector, I have basically just changed the 
 ## mean into inverse. You want to use the <<- instead of the <- when assigning y to x because you are
 ## assigning a value to an object in a different environment that you are in currently.
 
@@ -41,3 +41,6 @@ cacheSolve <- function(x, ...) {
   return(matrixinv)
   
 }
+
+## I tested it, by creating a square matrix, then running that one through makeCachematrix and lastly through 
+## cacheSolve. It seemed to work :) 
